c94470ca70c46193a3973963c1e6931abebdb04e
Shiva Sunar <shivasnr41@gmail.com>
2024-05-26 14:19:00 +0545

    minor fix

--- public/index.php
+++ public/index.php
@@ -24,16 +24,16 @@
         <?php
 		ini_set("display_errors", 1);
         require __DIR__.'/vendor/autoload.php';
-        $greeting = new \App\Greeting();
+        //$greeting = new \App\Greeting();
         
-        // $log = new Monolog\Logger('name');
-        // $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
-        // $log->warning('Foo');
+        $log = new Monolog\Logger('name');
+        $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+        $log->warning('Foo');
         ?>
 		<div class="site-content">
 			<main class="main-content">
 				<div class="container shrinked">
-					<h3 class="section-title center-aligned"><span><?php echo $greeting->sayWeAreComing(); ?></span></h3>
+					<h3 class="section-title center-aligned"><span><?php //echo $greeting->sayWeAreComing(); ?></span></h3>
 					<h1 class="section-title center-aligned">
 						We are preparing something new. <br/><br/>
 						<span>Stay tuned!</span>
